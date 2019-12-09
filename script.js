ee317ee9a8bed7982816edaade95f53f738435ce
J-Tech-Foundation <52475982+J-Tech-Foundation@users.noreply.github.com>
2019-12-09 07:40:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,131 @@
-//our friendly Definitions...
+// ==UserScript==
+// @name       glitch support dark mode
+// @namespace  SUPPORT-DARK-MODE
+// @version    0.1
+// @match      https://support.glitch.com/*
+// @description your friendly dark screen for support.glitch.com
+// ==/UserScript==
 
-var Dark = document.createElement("button");
-var Light = document.createElement("button");
-//event Listeners
-Dark.addEventListener("click", function(){
-  __a("LIGHT")
-  Dark.style.visibility = "hidden";
-  Light.style.visibility = "visible";
+
+//variables
+
+var body = document.getElementsByTagName("body")[0];
+var center = document.createElement("center");
+var hr = document.createElement("hr");
+var brk = document.createElement("br");
+var dark = document.createElement("button");
+var light = document.createElement("button");
+var style = document.createElement('style');
+
+var title = document.getElementsByClassName("fancy-title")[0];
+var category = document.getElementsByClassName("category-name");
+var user = document.getElementsByClassName("username");
+
+// specifying IDs so that I can declare styles
+
+dark.setAttribute("id", "dark");
+light.setAttribute("id", "light");
+
+
+//child appends
+
+center.appendChild(hr);
+center.appendChild(dark);
+// center.appendChild(brk);
+center.appendChild(light);
+
+body.appendChild(center);
+
+document.head.appendChild(style);
+
+// Setting innerHTML content
+
+dark.innerHTML = "dark";
+light.innerHTML = "light";
+
+//Document Event Listener
+
+document.addEventListener("ready", function() {
+  light.style.visibility = "hidden";
+  dark.style.visibility = "visible";
 });
-Light.addEventListener("click", function(){
+dark.addEventListener("click", function() {
   __a("DARK");
-  Light.style.visibility = "hidden";
-  Dark.style.visibility = "visible";
+  dark.style.visibility = "hidden";
+  light.style.visibility = "visible";
+});
+light.addEventListener("click", function() {
+  __a("LIGHT");
+  dark.style.visibility = "visible";
+  light.style.visibility = "hidden";
 });
 
-Dark.innerHTML = "Light Mode"
-Light.innerHTML = "Dark Mode"
 
-
-
-
-
-
-
-
-//our glorious function
-function __a (mode) {
-  switch (mode){
+//mode function
+function __a(mode) {
+  switch (mode) {
     case "DARK":
-      document.getElementsByTagName("body")[0].style.backgroundColor = "black"
+      body.style.backgroundColor = "black";
+      body.style.color = "white";
+      title.style.color = "white";
+      category.style.color = "white";
+      user.style.color = "white";
       break;
     case "LIGHT":
-      document.getElementsByTagName("body")[0].style.backgroundColor = "white"
+      body.style.backgroundColor = "white";
+      body.style.color = "black";
       break;
     default:
-      document.getElementsByTagName("body")[0].style.backgroundColor = "white"
-      break;//just for formality
+      body.style.backgroundColor = "white";
+      body.style.color = "black";
+      break;
   }
 }
+
+
+// Styles for the button
+
+style.innerHTML = `
+  
+#light {
+  border: solid #4545CC 2px;
+  color: #4545CC;
+  fill: #4545CC;
+  background: white;
+  height: 30px;  
+  border-radius: 5px;
+  transition: top 0.2s ease;
+  position: relative;
+  top: 0;
+  vertical-align: middle;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-family: monospace;
+  font-weight: bold;
+  margin-bottom: 40px;
+}
+
+#dark {
+  border: solid white 2px;
+  color: white;
+  fill: #4545CC;
+  background: #4545CC;
+  height: 32px;  
+  border-radius: 5px;
+  transition: top 0.2s ease;
+  position: relative;
+  top: 0;
+  vertical-align: middle;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-family: monospace;
+  font-weight: bold;
+  margin-bottom: 40px;
+  margin-right: 10px;
+}
+
+.fancy-title {
+  color: white;
+}
+
+`
